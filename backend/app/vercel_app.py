9c9f9f2109f76a9719a729288857b224669b0337
Atharva2099 <93972767+Atharva2099@users.noreply.github.com>
2024-10-20 10:59:47 -0700

    Update vercel_app.py

--- backend/app/vercel_app.py
+++ backend/app/vercel_app.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
+# Add the project root to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from mangum import Mangum
-from .main import app
+from backend.app.main import app
 
 handler = Mangum(app)
